c962cd4437240d874a2250fa18b0963bf716e21f
attoas <attoas@gmail.com>
2020-09-28 09:49:11 +0700

    Exercise4_Krishna_K.py

---
+++ Exercise4_Krishna_K.py
@@ -0,0 +1,22 @@
+# Foundation English
+xScoreFoundationEnglish = 95
+
+# General Business
+xScoreGeneralBusiness = 80
+
+# Introduction to Computer Systems
+xScoreComputerSys = 99
+
+# Computer Programming
+xScoreComputerProgramming = 60
+
+print ("======= Year 2 Semester 1 ===========")
+print  ("------ Your Score --------")
+print ("Foundation English :", 10," Points")
+print ("Foundation English :", xScoreFoundationEnglish," Points")
+print ("General Business :", 10, "Points")
+print ("Introduction to Computer Systems :",xScoreComputerSys, " Points")
+print ("Computer Programming :", xScoreComputerProgramming, " Points")
+
+print ("======= GOOD LUCK!! ===========")
+
